4d216f18ff584f46e70a4eeba0dc73bbc78ac6f7
Abhijnan <abhijnan2002@gmail.com>
2025-05-01 13:52:04 +0530

    update

--- ToDoApp/src/app/app.component.ts
+++ ToDoApp/src/app/app.component.ts
@@ -14,12 +14,43 @@ export class AppComponent {
   task='';
   taskList:{id:number,desc:string}[]=[];
 
-  addtask(){
-    this.taskList.push({id:this.taskList.length,desc:this.task});
+    // For editing
+  isEditing: boolean = false;
+  editingTaskId: number | null = null;
+
+  
+  // addtask(){
+  //    this.taskList.push({id:this.taskList.length,desc:this.task});
+
+  //    this.task = ''; // Clear input
+  // }
+
+
+  addtask() {
+    if (this.isEditing && this.editingTaskId !== null) {
+      const index = this.taskList.findIndex((t) => t.id === this.editingTaskId); // Finding Index 
+      // Checking tasklist empty or not 
+      if (index > -1) {                         
+        this.taskList[index].desc = this.task;
+      }
+      this.isEditing = false;
+      this.editingTaskId = null;
+    } else {
+      this.taskList.push({ id: this.taskList.length, desc: this.task });  // Adding Task
+    }
+
+    this.task = ''; // Clear input
   }
 
   deletetask(id:number){
     this.taskList =this.taskList.filter((item)=>item.id!=id);
   }
+
+  edittask(task: { id: number; desc: string }) {
+    this.task = task.desc;
+    this.editingTaskId = task.id;
+    this.isEditing = true;
+  }
+
 }
 
